0fa88dbea70261b0e3cf73118156866760f4d2e3
Mandelai <ricardo.aidelman@gmail.com>
2019-07-26 20:09:27 -0300

    Otro mas

--- Biblioteca/settings.py
+++ Biblioteca/settings.py
@@ -79,24 +79,25 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'biblioteca',
-        'USER': 'postgres',
-        'PASSWORD': 'RicSanCyn3154',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'biblioteca',
+#         'USER': 'postgres',
+#         'PASSWORD': 'RicSanCyn3154',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # 
 # Password validation
